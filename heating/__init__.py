cd490573c376a2c116bd272620160b6d2205c3eb
Ryan Kemper <ryankemper.cs@gmail.com>
2020-10-14 09:48:07 -0700

    Write a function to handle comparison logic

--- heating/__init__.py
+++ heating/__init__.py
@@ -11,6 +11,22 @@ import requests
 
 
 class HeatingManagerImpl:
+    @staticmethod
+    def decide_required_action(current_temp: float, threshold_temp: float, is_active: bool) -> str:
+        if not is_active:
+            return "no action needed"
+        else:
+            return "on" if current_temp < threshold_temp else "off"
+
+    ''' Observations about the initial design of this method:
+
+        It's doing too many things at once.
+
+        This method is responsible for doing the comparison operations to decide on/off, but also is responsible for opening sockets and sending the actual decision.
+        This means the comparison logic cannot be separately unit-tested because the function is trying to open a socket.
+
+        Finally as a nit, the function is immediately converting strings to floats, which means there is no error handling if a bad string gets passed in.
+    '''
     def manage_heating(self, t: str, threshold: str, active: bool):
         f_t = float(t)
         f_threshold = float(threshold)
@@ -54,3 +70,6 @@ class ScheduleManager:
 # Actually using the class
 heat_man_impl = HeatingManagerImpl()
 heat_man_impl.manage_heating("20", "25", True)
+
+print(HeatingManagerImpl.decide_required_action(20, 25, True))
+print(HeatingManagerImpl.decide_required_action(20, 25, False))
